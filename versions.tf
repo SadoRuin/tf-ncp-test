8e0c0fbe11688ae6ed9c4cdebde53410a1f51e92
박윤환(Park Yunhwan) <47595515+SadoRuin@users.noreply.github.com>
2023-11-29 10:59:53 +0900

    Rename the file provider.tf to versions.tf and move the provider block to main.tf

--- versions.tf
+++ versions.tf
@@ -16,12 +16,3 @@ terraform {
     }
   }
 }
-
-// Configure the ncloud provider
-provider "ncloud" {
-  access_key  = var.access_key
-  secret_key  = var.secret_key
-  region      = var.region
-  site        = var.site
-  support_vpc = var.support_vpc
-}
